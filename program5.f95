5296fea2e830bd27a2fc889c06ebbd159cf894fd
AlbertoPMDM <albertopmdm@gmail.com>
2022-01-28 22:06:24 -0600

    updated program5

--- program5.f95
+++ program5.f95
@@ -1,4 +1,4 @@
-!Alberto Méndez
+!Alberto Méndez 20220119
 
 ! programa que declare dos vectores de tres posciciones (v1 y v2) y tres matrices 2x2 que se llamen abc
 
@@ -19,31 +19,36 @@ program program5
 
      real, dimension(2,2) :: a, b, c
 
-     integer :: i, j
-
      v1(1) = 5
      v1(2) = 10
      v1(3) = 15
 
-     do i = 1, 3
-         v2(i) = 20
-     end do
+     v2(1) = 20
+     v2(2) = 20
+     v2(3) = 20
 
      a(1,1) = 1
      a(1,2) = 2
      a(2,1) = 3
      a(2,2) = 4
 
-     do i = 1, 2
-         do j = 1, 2
-             b(i,j) = 2
-         end do
-     end do
+     b(1,1) = 2
+     b(1,2) = 2
+     b(2,1) = 2
+     b(2,2) = 2
 
-     do i = 1, 2
-         do j = 1, 2
-             c(i,j) = a(i,j)/b(i,j)
-         end do
-     end do
+     c(1,1) = a(1,1)/b(1,1)
+     c(1,2) = a(1,2)/b(1,2)
+     c(2,1) = a(2,1)/b(2,1)
+     c(2,2) = a(2,2)/b(2,2)
 
+     print *, "v1 = [", v1(1), ", ", v1(2), ", ", v1(3), "]"
+
+     print *, "v2_2 = ", v2(2)
+
+     print *, "a_(1,1) = ", a(1,1)
+
+     print *, "b_(2,2) = ", b(2,2)
+
+     print *, "c = [ [", c(1,1), ", ", c(1,2), "], [", c(2,1), ", ", c(2,2), "] ]"
 end program program5
