722063f0874e6dfee1e49ffc4d3d7ee2896a3fd8
AlbertoPMDM <albertopmdm@gmail.com>
2022-02-15 22:31:05 -0600

    program15

---
+++ program15.f95
@@ -0,0 +1,31 @@
+!Alberto Méndez 20220215
+
+! Haga un programa que calcule los 
+! nums de la serie de fibonacci hasta un val límite que dé e ususario
+
+program program15
+    implicit none
+    
+    integer :: n, nprev, sum, lim, i, flag
+
+    print *, "Ingrese un numero entero para limitar la serie de fibonacci"
+    read (*,*) lim
+
+    ! cantidades iniciales para que la secuencia comience desde 0
+    nprev = -1
+    n = 1
+
+    ! alternatiavmente, en vez de poner un break, se puede utilizar la variable de cambio con -1 para salir del ciclo
+    
+    do
+        sum = nprev + n
+        nprev = n
+        n = sum
+        print *, n
+
+        if (n >= lim) then
+            exit
+        end if
+    end do
+
+end program program15
