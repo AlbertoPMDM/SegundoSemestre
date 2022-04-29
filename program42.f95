8a19591ffaf2218425d7daa2199c04cd2ad0da20
AlbertoPMDM <albertopmdm@gmail.com>
2022-04-29 13:32:29 -0500

    simpsons rule 8/3

---
+++ program42.f95
@@ -0,0 +1,135 @@
+! ALberto mendez 20222704
+! Trapezoid rule
+
+! 3
+
+! • Fecha: miércoles 27 de abril de 2022. • Hora: 04:30pm. • Nombre completo del ponente: M.C. Andrea González Báez. • Para validar tu asistencia AFI en la conferencia " Conceptualizando el bienestar de nuestras mascotas", llena este formulario: https://forms.gle/MmkSgUbTAe1tj5rFA
+module utils
+    implicit none
+    
+contains
+    function delta(a, b, n) result(dx)
+        implicit none
+        real*8, intent(in) :: a, b
+        integer, intent(in):: n
+        real*8 :: dx
+        
+        dx = (b-a)/n
+    end function delta
+
+    function partition(a, b, n) result(tmp_list)
+        implicit none
+        real*8, intent(in) :: a, b
+        integer, intent(in) :: n
+        real*8 :: dx
+        real*8, dimension(n+1) :: tmp_list
+        integer :: i
+        
+        dx = delta(a, b, n)
+
+        do i = 1, n
+            tmp_list(i) = a + (i-1)*dx
+        end do
+    end function partition
+
+    function evaluate(cfs, x) result(eval)
+        implicit none
+        real*8, dimension(:), intent(in) :: cfs
+        real*8, intent(in) :: x
+        real*8 :: eval
+        integer :: i
+
+        eval = 0
+        
+        do i = 1, SIZE(cfs)
+            eval = eval + cfs(i)*(x**ABS(i-SIZE(cfs)))
+        end do
+    end function evaluate
+
+    function poly3_integrate(a, b, n, cfs) result(approx)
+        implicit none
+        real*8, intent(in) :: a, b
+        integer, intent(in) :: n
+        real*8, intent(in), dimension(:) :: cfs
+        real*8 :: approx, insum
+        real*8, allocatable, dimension(:) :: p
+        integer :: i, n_new
+
+        n_new = n*3
+        insum = evaluate(cfs, a) + evaluate(cfs, b)
+        p = partition(a, b, n_new)
+        do i = 2, SIZE(p)-1
+            if ( mod(i,3)==0 ) then
+                insum = insum + 2*evaluate(cfs, p(i))
+            else
+                insum = insum + 3*evaluate(cfs, p(i))
+            end if
+        end do
+
+        approx = (3*delta(a, b, n_new)/8)*insum 
+    end function poly3_integrate
+
+    function append(element, array) result(a_array)
+        ! UWU
+        implicit none
+        real*8, allocatable, dimension(:), intent(in) :: array
+        real*8, intent(in) :: element
+        real*8, allocatable, dimension(:):: a_array
+        real*8, allocatable, dimension(:) :: tmp_array
+        integer :: i
+
+        if (allocated(array)) then
+
+            a_array = array
+
+            call move_alloc(a_array, tmp_array)
+
+            allocate(a_array(SIZE(array)+1))
+
+            do i=1, size(array)
+                a_array(i) = tmp_array(i)
+            end do
+
+            a_array(size(a_array)) = element
+
+        else
+
+            allocate(a_array(1))
+            a_array(1) = element
+
+        end if
+            
+        
+    end function append
+end module utils
+
+program program40
+    use utils
+    implicit none
+
+    real*8, allocatable, dimension(:) :: cfs
+    real*8 :: a, b
+    integer :: g, i, n
+    real*8 :: a_tmp
+
+    print *, 'ingresa el grado del polinomio: '
+    read (*,*) g
+
+    do i = 1, g+1
+        print *, 'introduce el coeficiente de x^', ABS(i-g-1), ': '
+        read (*,*) a_tmp
+        cfs = append(a_tmp, cfs)
+    end do
+
+    print *, 'introduce el limite inferior de integracion: '
+    read(*,*) a
+    print *, 'introduce el limite superior de integracion: '
+    read(*,*) b
+
+    print *, 'introduce el numero de particiones (se multiplicara por 3)'
+    read(*,*) n
+
+    print *, 'la integral en el rango dado es aproximadamente: '
+    print *, poly3_integrate(a, b, n, cfs)
+
+end program program40
---
+++ program42.py
@@ -0,0 +1,62 @@
+# ALberto mendez 20222704
+# Trapezoid rule
+
+class utils:
+
+    def abs(x):
+        return x if x>0 else -x
+
+    def delta(a:float, b:float, n:int) -> float:
+        return (b-a)/n
+
+    def partition(a:float, b:float, n:int) -> list:
+        tmp_list = []
+        dx = utils.delta(a, b, n)
+        for i in range(n):
+            tmp_list.append(a + i * dx)
+        return tmp_list
+
+    def evaluate(cfs:list, x:float) -> float:
+
+        eval = 0
+
+        for i in range(len(cfs)):
+            eval += cfs[i]*(x**utils.abs(i-len(cfs)+1))
+
+        return eval
+
+    def poly3_integrate(a:float, b:float, n:int, cfs:list) -> float:
+        '''
+        1 3 3 1
+              1 3 3 1
+                    1 3 3 1
+        1 3 3 2 3 3 2 3 3 1 
+        '''
+        n *= 3
+        insum = utils.evaluate(cfs, a) + utils.evaluate(cfs, b)
+        p = utils.partition(a, b, n)
+        for x in range(1,len(p)):
+            if x % 3 == 0:
+                insum += 2*utils.evaluate(cfs, p[x])
+            else:
+                insum += 3*utils.evaluate(cfs, p[x])
+
+        return (3*utils.delta(a, b, n)/8)* insum  
+  
+if __name__ == '__main__':
+
+    a = []
+
+    g = int(input('introduce el grado del polinomio \n'))
+    for i in range(g+1):
+        a.append(float(input('introduce el coeficiente de x^{} del polinomio\n'.format(-1*(i-g)))))
+
+    r=[]
+    r.append(float(input('introduce el limite inferior de integracion\n')))
+    r.append(float(input('introduce el limite superior de integracion\n')))
+
+    n = int(input('introduce el numero de particiones a utilizar (se multiplicara por 3)\n'))
+
+    print(f'''la integral cerca de x = {r[0]} a x = {r[1]} es aproximadamente {
+        utils.poly3_integrate(r[0], r[1], n, a)
+        }''')
